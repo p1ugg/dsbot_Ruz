108bd858ccde6a73a9658051d372734a1b7b1cde
Gazzaev Yaroslav Eduardovich <iegazzaev@edu.hse.ru>
2024-05-12 20:06:56 +0300

    with main

---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+
+from open_list_of_educators import get_names
+from tk import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.send('pong')
+
+
+@bot.command()
+async def get_list(ctx):
+    names = get_names()
+    await ctx.send(names)
+
+
+@bot.command()
+async def get_file(ctx):
+    def check_func(message: discord.Message) -> bool:
+        return message.author == ctx.author and message.content in ("1", "2", "3")
+
+    keys_of_materials = {
+        'Все лекции за 1 семестр | АИП': "./data/materials/lektsii1semestr.pdf",
+        'Все лекции за 2 семестр | АИП': "./data/materials/lektsii2semestr.pdf",
+        'Лабароторные работы 3 модуль | АИП': "./data/materials/laba3modul.docx",
+    }
+
+    list_of_keys = """"""
+
+    for i in range(len(list(keys_of_materials.keys()))):
+        list_of_keys = list_of_keys + str(i + 1) + ". " + list(keys_of_materials)[i] + '\n'
+
+    mes = "Выберите материал: \n" + list_of_keys
+    await ctx.send(mes)
+    user_message: discord.Message = await bot.wait_for('message', check=check_func, timeout=None)
+    if user_message.content == "1":
+        spr_file = "./data/materials/lektsii1semestr.pdf"
+    elif user_message.content == "2":
+        spr_file = "./data/materials/lektsii2semestr.pdf"
+    else:
+        spr_file = "./data/materials/laba3modul.docx"
+    await ctx.send(file=discord.File(spr_file))
+
+    # await user_message.reply(f'Hello, {user_message.author.mention}!')
+
+
+bot.run(token)
---
+++ ruz.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from pprint import pprint
+import json
+from datetime import date, timedelta
+import formater
+
+
+main_url = "https://ruz.hse.ru"
+
+
+def get_students_list(student_name):
+    params = { 'term': student_name,
+               'type': 'student'}
+    url = main_url+'/api/search'
+    r = requests.get(url, params=params)
+    return json.loads(r.text)
+
+
+def get_student_schedule(email, start_date, end_date, lng): # ('254711', '2020.09.28','2020.10.04', 1)
+    params = { 'start': start_date,
+               'end': end_date,
+               'email': email
+               }
+    headers = {"Accept-Language": "ru-RU, ru;q=0.9"}
+    url = 'https://api.hseapp.ru/v3/ruz/lessons'
+    r = requests.get(url, params=params, headers=headers)
+    return json.loads(r.text)
+
+a = get_student_schedule("iegazzaev@edu.hse.ru", "2024.05.13", "2024.05.13", 1)
+pprint(a)
+print(formater.format_schedule_one_day(a))
+
+def get_group_schedule(group_id, start_date, end_date, lng): # ('254711', '2020.09.28','2020.10.04', 1)
+    params = { 'start': start_date,
+               'end': end_date,
+               'group': group_id
+               }
+    headers = {"Accept-Language": "ru-RU, ru;q=0.9"}
+    url = 'https://api.hseapp.ru/v3/ruz/lessons'
+    r = requests.get(url, params=params, headers=headers)
+    return json.loads(r.text)
+
+
+def get_groups_list(group_name):
+    # https://ruz.hse.ru/api/search?term=ББИ2008&type=group
+    params = { 'term': group_name,
+               'type': 'group'}
+    url = 'https://ruz.hse.ru/api/search'
+    r = requests.get(url, params=params)
+    return json.loads(r.text)
+
+
+
+
+
+def email_is_valid(email):
+    """
+    Определяет, существует ли такой EMAIL,
+    отправляя запрос на получение его расписания
+    """
+    try:
+        email = email.strip()
+        test_date = date.today()
+        test_date_f= test_date.strftime("%Y.%m.%d")
+        resp = get_student_schedule(email, test_date_f, test_date_f, 1)
+        resp['error']
+        return False
+    except Exception as e:
+        return True
